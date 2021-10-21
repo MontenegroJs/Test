3426369cb5cbd73e62ab6aee41c48d9f9895f52c
MontenegroJs <92865798+MontenegroJs@users.noreply.github.com>
2021-10-21 16:33:31 -0500

    Add files via upload

---
+++ assets/js/Carga.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const Contentload = document.querySelector('.Contenedor-Cargando')
+    Contentload.style.opacity = 0
+    Contentload.style.visibility = 'hidden'
+})
