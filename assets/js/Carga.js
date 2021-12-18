736b7cb11dd1824074eb8bcca796d415ce0c4a7b
MontenegroJs <92865798+MontenegroJs@users.noreply.github.com>
2021-12-17 21:02:59 -0500

    Delete assets directory

--- assets/js/Carga.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-    const Contentload = document.querySelector('.Contenedor-Cargando')
-    Contentload.style.opacity = 0
-    Contentload.style.visibility = 'hidden'
-})
